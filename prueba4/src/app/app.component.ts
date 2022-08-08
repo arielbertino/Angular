daf3e51e3d9406c9a0fff1ce291ea3e46488ef6b
arielbertino <eugenioingenio10@gmail.com>
2022-08-08 16:59:04 -0300

    prueba4 Otro proyecto mas para incorporar concepto

--- prueba4/src/app/app.component.ts
+++ prueba4/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba4';
+  description = 'Me vuelvo locardo';
+
+  palabra: string="";
+  edad: Number =0;
+  soltero: Boolean = false;
+  // Tipo de datos any pueden recibir cualquier tipo de dato
+  // puede haber incosistencias de tipo o de operacion entre tipos
+  datos: any;
+  amigos: Array<any> =[];
 }
